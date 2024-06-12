c6012406cf8e629444501833f8af09434d27463b
Djalves424 <djalves424@gmail.com>
2024-06-12 15:39:56 -0300

    Project initial

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Catalog from "./routes/Catalog"
+
+export default function App() {
+  
+  return (
+  
+    <Catalog />
+  )
+}
---
+++ src/routes/Catalog/index.tsx
@@ -0,0 +1,8 @@
+import 'styles.css';
+
+export default function Catalog() {
+     return (
+        
+
+     );
+}
